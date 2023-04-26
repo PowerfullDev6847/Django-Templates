35b874f9d860b421895a19aa6567d867b03eb119
PowerfullDev6847 <ahmadhakimov6847@gmail.com>
2023-04-26 06:38:07 +0500

    Furniture

---
+++ main/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request=request, template_name="main/index.html")
+
+def about(request):
+    return render(request=request, template_name="main/about.html")
+
+def shop(request):
+    return render(request=request, template_name="main/shop.html")
+
+def furniture(request):
+    return render(request=request, template_name="main/furniture.html")
+
+def contact(request):
+    if request.method == "POST":
+        print(request.POST)
+
+    return render(request=request, template_name="main/contact.html")
+  
